6b83cb07b1960d746e51b8444e7f008697f8cf12
arul1309 <arulpradeep5@gmail.com>
2024-02-15 16:55:03 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+function addDot(number) {
+    var pinDisplay = document.getElementById("pinDisplay");
+    var dot = document.createElement("span");
+    dot.textContent = "●"; 
+    dot.classList.add("dot");
+    pinDisplay.appendChild(dot);
+}
+
+function removeDot() {
+    var pinDisplay = document.getElementById("pinDisplay");
+    pinDisplay.removeChild(pinDisplay.lastChild);
+}
+
+function clearPin() {
+    var pinDisplay = document.getElementById("pinDisplay");
+    pinDisplay.textContent = ""; 
+}
+
+function login() {
+    var pinDisplay = document.getElementById("pinDisplay");
+    pinDisplay.textContent = ""; 
+    alert("Login Successful..!");
+}
